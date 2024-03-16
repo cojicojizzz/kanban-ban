9fdc665dabb3947671e9c793de7eeadc7d7641e0
Kmuranaga <villageko@gmail.com>
2024-03-16 13:15:32 +0900

    [fix] コントローラー修正

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,9 @@
 class CardController < ApplicationController
+  # newアクションの前にset_listを実行
+  before_action :set_list, only: :new
+
   def new
     @card = Card.new
-    @list = List.find_by(id: params[:list_id])
   end
 
   def create
@@ -9,6 +11,8 @@ class CardController < ApplicationController
     if @card.save
       redirect_to :root
     else
+      # エラーが発生した場合はnew.html.erbを表示させるため、再度set_listを実行
+      set_list
       render action: :new
     end
   end
@@ -17,4 +21,9 @@ class CardController < ApplicationController
     def card_params
       params.require(:card).permit(:title, :memo, :list_id)
     end
+
+  # リストのIDを元にリストを取得
+  def set_list
+    @list = List.find_by(id: params[:list_id])
+  end
 end
